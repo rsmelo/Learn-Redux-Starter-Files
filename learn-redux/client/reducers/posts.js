c20f83a7fbdb4fdd7c79e415d654bb7e4982d4c9
rsmelo <rodrigosm87@gmail.com>
2016-07-26 10:59:58 -0300

    video 12 exercises

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -1,7 +1,16 @@
 const posts = (state = [], action) => {
-  console.log("post change");
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      console.log("incrementings likes");
+      const i = action.index;
+      return [
+        ...state.slice(0,i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i + 1)
+      ];
+    default:
+      return state;
+  }
 };
 
 export default posts;
